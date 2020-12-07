31be47fffbe7142e682d392e9669ea7d10d37863
Michael Ellis <michael.f.ellis@gmail.com>
2020-12-07 14:36:28 -0500

    Add cancellations and test. Now passing at 100% coverage.

--- pkg.go
+++ pkg.go
@@ -3,22 +3,50 @@ package pipe
 
 // PipeLineFuncs must read from inchan until it closes
 // and write to outchan, closing it when done. PipeLineFuncs
-// should panic on errors that can't be fixed within
-// the func.
-type Inlet func(chout chan (interface{}))
-type Section func(chin, chout chan (interface{}))
-type Outlet func(chin chan (interface{}), done chan (struct{}))
+// should call c.Cancel if an unrecoverable error occurs
+type Inlet func(chout PipeChan, c *Cancellation)
+type Section func(chin, chout PipeChan, c *Cancellation)
+type Outlet func(chin PipeChan, done chan (struct{}), c *Cancellation)
+
+type Cancellation struct {
+	cancelled bool
+	err       error
+}
+type PipeChan chan (interface{})
+
+func (c *Cancellation) Cancel(err error) {
+	c.cancelled = true
+	c.err = err
+}
+
+func (c *Cancellation) Cancelled() bool {
+	return c.cancelled
+}
 
 // PipeLine runs a pipeline.
-func PipeLine(inlet Inlet, outlet Outlet, sections ...Section) {
+func PipeLine(inlet Inlet, outlet Outlet, sections ...Section) (err error) {
+	var c Cancellation
+	// Provide the inlet function with an output channel
 	var chout = make(chan (interface{}), 1)
-	go inlet(chout)
-	for _, itemFunc := range sections {
+	// Launch it.
+	go inlet(chout, &c)
+	// Launch the internal pipeline sections
+	for _, section := range sections {
+		// Use the prior output channel as the input
 		chin := chout
+		// Make a new output channel
 		chout = make(chan (interface{}), 1)
-		go itemFunc(chin, chout)
+		// Launch the current section
+		go section(chin, chout, &c)
 	}
+	// Make a "done" channel for the outlet
 	done := make(chan (struct{}), 1)
-	go outlet(chout, done)
+	// Launch it with the preceding output channel as its input.
+	go outlet(chout, done, &c)
+
+	// Block on the done channel (which the outlet function
+	// must close when all processing is complete.)
 	<-done
+	err = c.err
+	return
 }
--- pkg_test.go
+++ pkg_test.go
@@ -10,36 +10,75 @@ type Item struct {
 }
 
 func TestPipeLine(t *testing.T) {
-	PipeLine(inlet, outlet, section1, section2)
+	err := PipeLine(inlet, outlet, section1, section2)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+}
+func TestPipeLineError(t *testing.T) {
+	var streamingInlet Inlet = func(chout PipeChan, c *Cancellation) {
+		defer close(chout)
+		for {
+			if c.Cancelled() {
+				return
+			}
+			chout <- Item{s: "blah"}
+		}
+	}
+	var badSection Section = func(chin, chout PipeChan, c *Cancellation) {
+		c.Cancel(fmt.Errorf("I bailed."))
+		close(chout)
+	}
+	err := PipeLine(streamingInlet, outlet, section1, badSection, section2)
+	if err == nil {
+		t.Error("Expected failure but the error was nil")
+	}
+	exp := "I bailed."
+	got := err.Error()
+	if got != exp {
+		t.Errorf(`exp: "%v", got "%v"`, exp, got)
+	}
 }
 
-var inlet Inlet = func(outchan chan (interface{})) {
+var inlet Inlet = func(outchan PipeChan, c *Cancellation) {
+	defer close(outchan)
 	for _, s := range []string{"foo", "bar"} {
+		if c.Cancelled() {
+			return
+		}
 		outchan <- Item{s: s}
 	}
-	close(outchan)
 }
-var section1 Section = func(inchan, outchan chan (interface{})) {
+var section1 Section = func(inchan, outchan PipeChan, c *Cancellation) {
+	defer close(outchan)
 	for item := range inchan {
+		if c.Cancelled() {
+			return
+		}
 		it := item.(Item)
 		it.s += " stage1"
 		outchan <- it
 		outchan <- it
 
 	}
-	close(outchan)
 }
-var section2 Section = func(inchan, outchan chan (interface{})) {
+var section2 Section = func(inchan, outchan PipeChan, c *Cancellation) {
+	defer close(outchan)
 	for item := range inchan {
+		if c.Cancelled() {
+			return
+		}
 		it := item.(Item)
 		it.s += " stage2"
 		outchan <- it
 	}
-	close(outchan)
 }
 
-var outlet Outlet = func(inchan chan (interface{}), done chan (struct{})) {
+var outlet Outlet = func(inchan PipeChan, done chan (struct{}), c *Cancellation) {
 	for item := range inchan {
+		if c.Cancelled() {
+			return
+		}
 		it := item.(Item)
 		fmt.Println(it.s)
 	}
